79b988cd686ae7a34ac296c80f511dfdb528162c
User <User@WIN-DE1S8KB645B>
2026-02-24 19:00:04 +0300

    написал в изменения в Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+{
+    static void Main()
+    {
+        Zoo zoo = new Zoo();
+        zoo.AddAnimal(new Mammal("Лев", 5));
+        zoo.AddAnimal(new Bird("Орел", 3));
+        zoo.ShowAll();
+    }
+}
