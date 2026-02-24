5188e9ff95bb4982ddc4fdad0520baa75fede511
User <User@WIN-DE1S8KB645B>
2026-02-24 19:11:15 +0300

    добавил новые функции в Animal.cs

--- Animals.cs
+++ Animals.cs
@@ -2,15 +2,27 @@ public class Animal
 {
     private string _name;
     private int _age;
-    public string Name = _name;
-    public int Age = _age;
+    private int _soundCount;  
+
+    public string Name => _name;
+    public int Age => _age;
+
     public Animal(string name, int age)
     {
         _name = name;
         _age = age;
+        _soundCount = 0;  
     }
+
     public virtual void MakeSound()
     {
+        _soundCount++;  
         Console.WriteLine("Животное издает звук");
+    }
+
+    
+    public int GetSoundCount()
+    {
+        return _soundCount;
     }
 }
