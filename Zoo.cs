a747ff6ee36ce0fa76eeae51e33f442180dd5190
debug123666 <mikhail.rybkin.9090@mail.ru>
2026-02-24 18:54:31 +0300

    Добавил класс Zoo.cs

---
+++ Zoo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Zoo
+{
+    private List<Animal> _animals = new List<Animal>();
+    public void AddAnimal(Animal animal)
+    {
+        _animals.Add(animal);
+    }
+    public void ShowAll()
+    {
+        foreach (var animal in _animals)
+        {
+            animal.MakeSound();
+        }
+    }
+}
