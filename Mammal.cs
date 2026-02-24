1ba6a5e285fb34af0d7e0979af9e6f8977b8ca62
debug123666 <mikhail.rybkin.9090@mail.ru>
2026-02-24 18:26:09 +0300

    Добавил класс Mammal.cs

---
+++ Mammal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Mammal : Animal
+{
+    public Mammal(string name, int age) : base(name, age) { }
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} рычит");
+    }
+}
